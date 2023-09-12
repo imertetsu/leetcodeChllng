c62e1f98e7c27a06a64b0a624be0f7f87c394bbe
Imer Coaguila <imertpro@gmail.com>
2023-09-12 17:41:00 -0400

    merge arrays done

--- src/EasyPractices/MergeArrays.java
+++ src/EasyPractices/MergeArrays.java
@@ -1,15 +1,18 @@
 package EasyPractices;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MergeArrays {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(nums1.length == m + n){
-            for (int i = 0; i < nums2.length; i++){
-                nums1[n+i] = nums2[i];
-            }
-            System.out.println(Arrays.toString(nums1));
+
+        for (int j = 0, i = m; j < n; j++){
+            nums1[i] = nums2[j];
+            i++;
         }
+        System.out.println(Arrays.toString(nums1));
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ public class Main {
         System.out.println("Hello world!");
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
+
+        int[] nums3 = {};
+        int[] nums4 = {1};
         MergeArrays mergeArrays = new MergeArrays();
+        MergeArrays mergeArrays2 = new MergeArrays();
+
         mergeArrays.merge(nums1, 3, nums2, 3);
+        mergeArrays2.merge(nums3, 0, nums4, 1);
     }
 }
