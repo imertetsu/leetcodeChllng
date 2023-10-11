4cf11a4a3bcc3de3ce77375b8a4e08bf7227cd00
Imer Coaguila <imertpro@gmail.com>
2023-10-10 22:09:45 -0400

    backtracking on progress

--- src/EasyPractices/NumberCombination.java
+++ src/EasyPractices/NumberCombination.java
@@ -1,7 +1,23 @@
 package EasyPractices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberCombination {
-    public int[] combination(int[] numbers, int obj){
+    private List<List<Integer>> result;
+    public NumberCombination(){
+        result = new ArrayList<>();
+    }
+    public List<int[]> combination(int[] numbers, int obj){
+        List<Integer> combineList = new ArrayList<>();
+        //Caso base
+        if(obj == 0){
+            result.add(combineList);
+        }
+        if ()
+        //Caso fail
+
+        //Backtracking
         int [] result;
 
         return result;
