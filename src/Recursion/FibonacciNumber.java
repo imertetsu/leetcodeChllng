3b787ef7aacb2788ce5be9719f2c77953e5d9399
imertetsu <imertpro@gmail.com>
2024-05-28 09:46:17 -0400

    Recursion Fibonacci added

---
+++ src/Recursion/FibonacciNumber.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class FibonacciNumber {
+    public static int fib(int n) {
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(7));
+    }
+}
