c25aad55dbe1a034bdfb79e4145f66bffd9453c2
imertetsu <imertpro@gmail.com>
2024-05-29 12:37:33 -0400

    MergeTwoLinkedLists added

--- src/Recursion/FibonacciNumber.java
+++ src/Recursion/FibonacciNumber.java
@@ -2,15 +2,19 @@ package Recursion;
 
 public class FibonacciNumber {
     public static int fib(int n) {
-        if(n == 0)
-            return 0;
-        if(n == 1)
-            return 1;
+        if(n == 0 || n == 1)
+            return n;
 
         return fib(n - 1) + fib(n - 2);
     }
 
     public static void main(String[] args) {
         System.out.println(fib(7));
+        try{
+            System.out.println("123");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
---
+++ src/Recursion/MergeTwoSortedLists.java
@@ -0,0 +1,46 @@
+package Recursion;
+
+public class MergeTwoSortedLists {
+    /*
+    You are given the heads of two sorted linked lists list1 and list2.
+
+    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+    Return the head of the merged linked list.
+    Input: list1 = [1,2,4], list2 = [1,3,4]
+    Output: [1,1,2,3,4,4]
+    Example 2:
+
+    Input: list1 = [], list2 = []
+    Output: []
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if (list2 == null){
+            System.out.println(list1.val);
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+        ListNode mergedList = mergeTwoSortedLists.mergeTwoLists(list1, list2);
+
+        // Print the merged list
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " -> ");
+            mergedList = mergedList.next;
+        }
+        System.out.println("null");
+    }
+}
