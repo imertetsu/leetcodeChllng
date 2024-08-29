0143c1049db83586668973bd28e57248ccebfb86
imertetsu <imertpro@gmail.com>
2024-08-28 21:53:15 -0400

    primitiveTypes added .

---
+++ src/TestPrimitiveTypes/Test.java
@@ -0,0 +1,8 @@
+package TestPrimitiveTypes;
+
+public class Test {
+    public static void main(String[] args) {
+        byte num = (byte) 130;
+        System.out.println("El valor de num es: "+num);
+    }
+}
